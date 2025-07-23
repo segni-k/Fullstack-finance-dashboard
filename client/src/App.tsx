3800a3082b7b2f4220383b9fe33635c337f5fcf8
segni-k <segnikasahun969@gmail.com>
2025-07-23 21:41:49 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,11 @@
 function App() {
   return (
     <div className="app">
-      <h1>finance Dashboard</h1>
+      <h1>finance</h1>
      
      
     </div>
   )
 }
-
+looy4liol3ollyo453ouu53u5olloo36jjhy
 export default App

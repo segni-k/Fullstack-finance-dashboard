d165c048992f0f19144926e7ae8f54654945e32d
segni-k <segnikasahun969@gmail.com>
2026-01-13 23:18:51 +0300

    Editing App component
    Removed duplicate headings from the App component.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,6 @@ function App() {
   return (
     <div className="app">
       <h1>Finance Dashboards</h1>
-      <h2>Finance Dashboards</h2>
-      <h3>Finance Dashboards</h3>
      
      
     </div>
